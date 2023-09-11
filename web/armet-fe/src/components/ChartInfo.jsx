c4a752e78c2d863fa212ee731c9fb9c278c71378
Stefan Knott <stefan@replicated.com>
2023-09-11 11:25:14 -0600

    add status icon

--- web/armet-fe/src/components/ChartInfo.jsx
+++ web/armet-fe/src/components/ChartInfo.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const ChartInfo = (props) => {
     return (
         <div>
-        <h2>{props.chart.chart.metadata.name}</h2>
+        <div >
+        <h1 className="chartName">{props.chart.chart.metadata.name}</h1>
+        {props.chart.info.status === "deployed" &&
+            <div className="deployed"/>
+        }
+         {props.chart.info.status != "deployed" &&
+            <div className="failed"/>
+        }
+        </div>
         <p>Chart version: {props.chart.chart.metadata.version}</p>
         <p>Application version: {props.chart.chart.metadata.appVersion}</p>
         <p>Status: {props.chart.info.status}</p>
